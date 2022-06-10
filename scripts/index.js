371dbed5e1287b711574f61fcab0257e040681b0
Alexandr Idgenov <cop2100@yandex.ru>
2022-06-10 16:55:28 +0700

    added image from js

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,6 @@
 const editButton = document.querySelector('.profile__info-edit-btn');
 
 const places = document.querySelector('.places');
-const likeButtons = document.querySelectorAll('.place__like-btn');
 
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close-btn');
@@ -13,6 +12,33 @@ const descriptionProfile = document.querySelector('.profile__info-subtitle');
 
 const nameEdit = document.querySelector('.popup__form-input_type_name-on');
 const descriptionEdit = document.querySelector('.popup__form-input_type_description-on');
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
 // открытие-закрытие popup
 function openClosePopup() {
@@ -29,32 +55,41 @@ function openClosePopup() {
   popup.classList.toggle('popup_opened');
 }
 
-// проверка области клика при закрытии окна
-function checkCloseClickPopup(e) {
-  if (e.target === e.currentTarget) {
+// проверка области клика при закрытии popup
+function checkCloseClickPopup(evt) {
+  if (evt.target === evt.currentTarget) {
     openClosePopup();
   }
 }
 
 // замена имени и описания в профиле
-function formSubmitHandler(e) {
-  e.preventDefault();
+function formSubmitHandler(evt) {
+  evt.preventDefault();
   nameProfile.textContent = nameEdit.value;
   descriptionProfile.textContent = descriptionEdit.value;
   openClosePopup();
 }
 
-// добавление лайка
-function addLike(e) {
-  likeButtons.forEach(element => {
-    if (e.target.contains(element)) {
-      e.target.classList.toggle('place__like-btn_active');
-    }
+// доюавление шаблона place
+function addPlace(namePlace, linkImage) {
+  const placeTemplate = document.querySelector('#place-template').content;
+  const placeElement = placeTemplate.querySelector('.place').cloneNode(true);
+
+  placeElement.querySelector('.place__image').src = linkImage;
+  placeElement.querySelector('.place__image').alt = 'Фото ' + namePlace;
+  placeElement.querySelector('.place__title').textContent = namePlace;
+  placeElement.querySelector('.place__like-btn').addEventListener('click', evt => {
+    evt.target.classList.toggle('place__like-btn_active');
   });
+
+  places.append(placeElement);
 }
+
+initialCards.forEach(element => {
+  addPlace(element.name, element.link);
+});
 
 editButton.addEventListener('click', openClosePopup);
 popupCloseButton.addEventListener('click', openClosePopup);
 popup.addEventListener('click', checkCloseClickPopup);
 formEdit.addEventListener('submit', formSubmitHandler);
-places.addEventListener('click', addLike);
