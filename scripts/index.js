c33d6b493826f7d7713175e65524f41a6cb34ffc
Alexandr Idgenov <cop2100@yandex.ru>
2022-08-02 13:08:00 +0700

    added class section js

--- scripts/index.js
+++ scripts/index.js
@@ -1,68 +1,42 @@
-import Card from './Card.js';
-import FormValidator from './FormValidator.js';
+import {  nameProfile,
+  descriptionProfile,
+  places,
+  btnEditProfile,
+  btnAddPlace,
+  popupEditProfile,
+  formEditProfile,
+  nameEdit,
+  descriptionEdit,
+  popupAddPlace,
+  formAddPlace,
+  namePlace,
+  linkForPlace,
+  btnSubmitPlace,
+  popupImagePlace,
+  imagePlace,
+  imagePlaceCaption,
+  settings,
+  initialCards } from './constants.js';
+import Section from './components/Section.js';
+import Card from './components/Card.js';
+import FormValidator from './components/FormValidator.js';
 
-const nameProfile = document.querySelector('.profile__info-title');
-const descriptionProfile = document.querySelector('.profile__info-subtitle');
+const placesList = new Section({
+  items: initialCards,
+  renderer: (item) => {
+    const card = new Card('#place-template', item, getPopupImage);
+    const cardPlace = card.generateCard();
 
-const places = document.querySelector('.places');
-
-const btnEditProfile = document.querySelector('.profile__info-edit-btn');
-const btnAddPlace = document.querySelector('.profile__add-btn');
-
-const popupEditProfile = document.querySelector('.popup_type_edit-profile');
-const formEditProfile = document.forms.formEditProfile;
-const nameEdit = formEditProfile.elements.profileName;
-const descriptionEdit = formEditProfile.elements.profileDescription;
-const btnSubmitProfile = formEditProfile.querySelector('.popup__form-save-btn');
-
-const popupAddPlace = document.querySelector('.popup_type_add-place');
-const formAddPlace = document.forms.formAddPlace;
-const namePlace = formAddPlace.elements.placeName;
-const linkForPlace = formAddPlace.elements.placeLink;
-const btnSubmitPlace = formAddPlace.querySelector('.popup__form-save-btn');
-
-const popupImagePlace = document.querySelector('.popup_type_image-place');
-const imagePlace = document.querySelector('.popup__image');
-const imagePlaceCaption = document.querySelector('.popup__image-caption');
-
-const settings = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__form-input',
-  submitButtonSelector: '.popup__form-save-btn',
-  inactiveButtonClass: 'popup__form-save-btn_type_no-active',
-  inputErrorClass: 'popup__form-input_type_error',
-  errorClass: 'popup__form-input-error_visible'
-};
+    placesList.addItem(cardPlace);
+  },
+},
+places
+);
 
 const checkEditProfile = new FormValidator(settings, popupEditProfile);
 const checkAddPlace = new FormValidator(settings, popupAddPlace);
 
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
+
 
 // открытие popup
 function openPopup(popup) {
@@ -159,7 +133,8 @@ function createCard(cardElement) {
   return cardPlace;
 }
 
-initialCards.forEach(element => renderCard(places, element));
+//initialCards.forEach(element => renderCard(places, element));
+placesList.render();
 
 btnEditProfile.addEventListener('click', getPopupEditProfile);
 btnAddPlace.addEventListener('click', getPopupAddPlace);
